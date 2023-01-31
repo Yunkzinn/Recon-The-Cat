3fee93adce79864f5c89d9c0ad97b48603f7f20b
Leozinn <60331681+Yunkzinn@users.noreply.github.com>
2023-01-31 11:38:57 -0300

    Bugs Fixed

--- scripts/endpoints.py
+++ scripts/endpoints.py
@@ -16,7 +16,7 @@ os.system(f"{httpx200} | gau --blacklist {blacklist} -o {gau}")
 os.system(f"{httpx200} | gauplus -o {gauplus}")
 os.system(f"{httpx200} | hakrawler | anew {hakrawler}")
 os.system(f"{httpx200} | xargs -P100 -I@ gospider -c 30 -t 15 -d 4 -a -H 'x-forwarded-for: 127.0.0.1' -H 'User-Agent: Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1' -s @ -o {gospider}")
-os.system(f"cat {katana} {gau} {gauplus} {hakrawler} {gospider} | sort -u {endpoints} -o {endpoints}")
+os.system(f"cat {katana} {gau} {gauplus} {hakrawler} {gospider} | anew {endpoints}")
 os.system(f"cat {endpoints} | httpx -silent -no-color -o {endpoints200}")
 
 #Next improvement add Paramspider
