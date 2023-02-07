7579600368614d4d93c97d71de1a351c8d5e4be1
Leozinn <60331681+Yunkzinn@users.noreply.github.com>
2023-02-06 23:41:31 -0300

    improvements

--- scripts/enum.py
+++ scripts/enum.py
@@ -10,12 +10,13 @@ amass2 = f"{domain.split('.')[0]}_amass2.txt"
 chaos = f"{domain.split('.')[0]}_chaos.txt"
 githubHttpx = f"{domain.split('.')[0]}_githubHttpx.txt"
 subfile = f"{domain.split('.')[0]}_subs.txt"
-#dnsgen = f"{domain.split('.')[0]}_dnsgen.txt"
+dnsgen = f"{domain.split('.')[0]}_dnsgen.txt"
+allSubs = f"{domain.split('.')[0]}_allSubs.txt"
 httpx = f"{domain.split('.')[0]}_httpx.txt"
 httpx200 = f"{domain.split('.')[0]}_200httpx.txt"
 awkHttpx = f"{domain.split('.')[0]}_awkhttpx.txt"
 apiGitHub = "YOUR_API_KEY_GITHUB"
-
+allports = "80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672"
 
 
 os.system(f"echo {domain} | haktrails subdomains >> {haktrails}")
@@ -26,10 +27,8 @@ os.system(f"amass enum -norecursive -brute -d {domain} -o {amass2}")
 os.system(f"chaos -d {domain} -silent -o {chaos}")
 os.system(f"python3 ~/Tools/github-search/github-subdomains.py -t {apiGitHub} -d {domain} | httpx --title >> {githubHttpx}")
 os.system(f"cat {haktrails} {assetfinder} {subfinder} {amass} {amass2} {chaos} {githubHttpx} | anew {subfile}")
-#os.system(f"cat {subfile} | dnsgen - | massdns -r ~/resolvers/resolvers.txt -t AAAA -o L ")
-os.system(f"cat {subfile} | httpx -silent -no-color -o {httpx}")
-os.system(f"cat {subfile} | httpx -status-code -mc 200 -silent -no-color | tr -d '[]' | anew {httpx200}; cat {httpx200} | awk '{{print $1}}' >> {awkHttpx}; rm -rf {httpx200}; mv {awkHttpx} {httpx200}")
-os.system(f"rm -rf {haktrails} {assetfinder} {subfinder} {amass} {amass2} {chaos} {githubHttpx}")
-
-
-#Dnsgen in tests
+os.system(f"cat {subfile} | dnsgen - | httpx -silent -no-color -p {allports} -o {dnsgen}")
+os.system(f"cat {subfile} {dnsgen} | anew {allSubs}")
+os.system(f"cat {allSubs} | httpx -silent -no-color -p {allports} -o {httpx}")
+os.system(f"cat {allSubs} | httpx -status-code -mc 200 -silent -no-color | tr -d '[]' | anew {httpx200}; cat {httpx200} | awk '{{print $1}}' >> {awkHttpx}; rm -rf {httpx200}; mv {awkHttpx} {httpx200}")
+os.system(f"rm -rf {haktrails} {assetfinder} {subfinder} {amass} {amass2} {chaos} {githubHttpx} {dnsgen} {subfile}")
