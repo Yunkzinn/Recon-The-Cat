1828bb4c73109c117d2d85e70b56224a85ac24fc
Leozinn <60331681+Yunkzinn@users.noreply.github.com>
2023-02-09 16:14:39 -0300

    .

--- scripts/jsAnalysis.py
+++ scripts/jsAnalysis.py
@@ -5,9 +5,4 @@ resultJSPASS = f"resultJSPASS"
 
 os.system(f"cat {aliveJustJS} | python3 ~/python-tools/collector.py output ; rush -i output/urls.txt 'python3 ~/python-tools/SecretFinder.py -i {{}} -o cli | sort -u >> output/{resultJSPASS}'")
 os.system(f"echo 'USE JSScanner https://github.com/0x240x23elu/JSScanner'")
-os.system(f"")
-os.system(f"")
-os.system(f"")
-os.system(f"")
-os.system(f"")
-os.system(f"")
+
