4b4edaaed75ac8d8f7c4da3ee8b47363493f215a
Leozinn <60331681+Yunkzinn@users.noreply.github.com>
2023-02-16 22:17:46 -0300

    Improvements

--- scripts/endpoints.py
+++ scripts/endpoints.py
@@ -6,20 +6,26 @@ gau = f"gauEndpoints"
 gauplus = f"gauplus"
 hakrawler = f"hakrawler"
 waybackurls = f"waybackurls"
-gospider = f"gospider"
+#gospider = f"gospider"
+paramspider = f"paramspider"
+arjun1 = f"arjun1"
+arjun2 = f"arjun2"
 endpoints = f"endpoints"
 endpoints200 = f"endpoints200"
 blacklist = "md,jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico"
 
 os.system(f"katana -list *_httpx200 -d 15 -silent -f qurl -o {katana}")
 os.system(f"{httpx200} | gau --blacklist {blacklist} --o {gau}")
-os.system(f"{httpx200} | gauplus -random-agent | uro | tee {gauplus}")
+os.system(f"{httpx200} | gauplus -b {blacklist} -random-agent | uro | tee {gauplus}")
 os.system(f"{httpx200} | hakrawler | uro | tee {hakrawler}")
-os.system(f"{httpx200} | waybackurls | uro| tee {waybackurls}")
-os.system(f"{httpx200} | xargs -P100 -I@ gospider -c 30 -t 15 -d 4 -a -H 'x-forwarded-for: 127.0.0.1' -H 'User-Agent: Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1' -s @ | tr ' ' '\n' | grep -v '.js' | grep 'https://' | grep '=' | tee {gospider}")
-os.system(f"cat {katana} {gau} {gauplus} {hakrawler} {waybackurls} {gospider} | uro | sort -u {endpoints}")
+os.system(f"{httpx200} | waybackurls | uro | tee {waybackurls}")
+#os.system(f"{httpx200} | xargs -P100 -I@ gospider -c 30 -t 15 -d 4 -a -H 'x-forwarded-for: 127.0.0.1' -H 'User-Agent: Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1' -s @ | tr ' ' '\n' | grep -v '.js' | grep 'https://' | grep '=' | tee {gospider}")
+os.system(f"xargs -a *_httpx200 -I@ bash -c 'python3 /Tools/ParamSpider -d @ --level high -e {blacklist}' | uro | tee {paramspider}")
+os.system(f"arjun -i *_httpx200 -oT {arjun1}; arjun -i *_httpx200 -m POST -oT {arjun2} ")
+os.system(f"cat {katana} {gau} {gauplus} {hakrawler} {waybackurls} {paramspider} {arjun1} {arjun2} | uro | sort -u | tee {endpoints}")
 os.system(f"cat {endpoints} | httpx -silent -no-color -o {endpoints200}")
-os.system(f"rm -rf {katana} {gau} {gauplus} {hakrawler} {waybackurls} {gospider}")
+os.system(f"rm -rf {katana} {gau} {gauplus} {hakrawler} {waybackurls} {paramspider} {arjun1} {arjun2}")
 os.system(f"echo 'Endpoints Module Finished' | notify ")
 
-#Next improvement add Paramspider
+#Gospider in tests
+#Waybackurls returns garbage | Need Improvements
--- scripts/js.py
+++ scripts/js.py
@@ -25,8 +25,8 @@ os.system(f"{httpx200} | gospider --js | grep -E '\.js(?:onp?)?$'' | awk '{{prin
 os.system(f"{httpx200} | xargs -I% -P10 sh -c 'hakrawler -plain -linkfinder -depth 5 -url %' | awk '{{print $3}}' | grep -E '\.js(?:onp?)?$' | anew {hakrawler1}")
 os.system(f"{httpx200} | rush -j 100 'hakrawler -js -plain -usewayback -depth 6 -scope subs -url {{}} | unew h {hakrawler2}'")
 os.system(f"{httpx200} | xargs -I@ sh -c 'python3 dirsearch.py -r -b -w path -u @ -i 200, 403, 401, 302 -e json,js,jsp' | tee {dirsearch}")
-os.system(f"cat {getJS} {katana} {gau} {waybackurls1} {waybackurls2} {gospider} {hakrawler1} {hakrawler2} {dirsearch} | sort -u {allJS}")
-os.system(f"cat {getJS} {katana} {gau} {waybackurls1} {waybackurls2} {gospider} {hakrawler1} {hakrawler2} {dirsearch} | grep -E '\.js(?:onp?)?$' | sort -u {justJS}")
+os.system(f"cat {getJS} {katana} {gau} {waybackurls1} {waybackurls2} {gospider} {hakrawler1} {hakrawler2} {dirsearch} | sort -u | tee {allJS}")
+os.system(f"cat {getJS} {katana} {gau} {waybackurls1} {waybackurls2} {gospider} {hakrawler1} {hakrawler2} {dirsearch} | grep -E '\.js(?:onp?)?$' | sort -u | tee {justJS}")
 os.system(f"cat {allJS} | anti-burl | anew {js200}")
 os.system(f"cat {justJS} | anti-burl | anew {aliveJustJS}")
 os.system(f"rm -rf {getJS} {katana} {gau} {waybackurls1} {waybackurls2} {gospider} {hakrawler1} {hakrawler2} {dirsearch}")
@@ -35,4 +35,4 @@ os.system(f"echo 'JS Module Finished' | notify ")
 
 
 #Add In Future:
-#os.system(f"cat dominios | gau | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> gauJS.txt ; cat dominios | waybackurls | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> waybJS.txt ; gospider -a -S dominios -d 2 | grep -Eo '(http|https)://[^/\"].*\.js+' | sed 's#\] \- #\n#g' >> gospiderJS.txt ; cat gauJS.txt waybJS.txt gospiderJS.txt | sort -u >> saidaJS ; rm -rf *.txt ; cat saidaJS | anti-burl |awk '{{print $4}}' | sort -u >> AliveJs.txt ; xargs -a AliveJs.txt -n 2 -I@ bash -c 'echo -e '\n[URL]: @\n'; python3 linkfinder.py -i @ -o cli' ; cat AliveJs.txt  | python3 collector.py output ; rush -i output/urls.txt 'python3 SecretFinder.py -i {{}} -o cli | sort -u >> output/resultJSPASS'")
+#os.system(f"cat dominios | gau | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> gauJS.txt ; cat dominios | waybackurls | grep -iE '\.js'| grep -iEv '(\.jsp|\.json)' >> waybJS.txt ; gospider -a -S dominios -d 2 | grep -Eo '(http|https)://[^/\"].*\.js+' | sed 's#\] \- #\n#g' >> gospiderJS.txt ; cat gauJS.txt waybJS.txt gospiderJS.txt | sort -u | tee >> saidaJS ; rm -rf *.txt ; cat saidaJS | anti-burl |awk '{{print $4}}' | sort -u | tee >> AliveJs.txt ; xargs -a AliveJs.txt -n 2 -I@ bash -c 'echo -e '\n[URL]: @\n'; python3 linkfinder.py -i @ -o cli' ; cat AliveJs.txt  | python3 collector.py output ; rush -i output/urls.txt 'python3 SecretFinder.py -i {{}} -o cli | sort -u | tee >> output/resultJSPASS'")
